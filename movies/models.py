614597dc10d2cdcf5e054da65c0a50c70f0273ab
miguel977 <miguel645@gmail.com>
2024-12-15 08:25:51 -0300

    m Criando o models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=60)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+
+    )
